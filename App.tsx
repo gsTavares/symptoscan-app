9737bc146df6913cace71661b0b25883b289dd69
gsTavares <gustavo.tavares24@outlook.com>
2023-04-01 19:24:43 -0300

    feat: add moti for animations

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
+import 'react-native-reanimated'
 import "react-native-gesture-handler"
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { useFonts, OpenSans_400Regular } from "@expo-google-fonts/open-sans";
+import { MotiView } from 'moti';
 
 export default function App() {
 
@@ -14,9 +16,9 @@ export default function App() {
   }
 
   return (
-    <View className="flex-1 items-center justify-center bg-slate-300">
+    <MotiView from={{opacity: 0}} animate={{opacity: 1}} transition={{type: "timing"}} className="flex-1 items-center justify-center bg-slate-300">
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </MotiView>
   );
 }
