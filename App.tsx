11e1c67f3df134fbdb7a728cadbe9988d33b5080
gsTavares <gustavo.tavares24@outlook.com>
2023-04-01 19:12:11 -0300

    feat: add expo-google-font open sans

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import "react-native-gesture-handler"
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { useFonts, OpenSans_400Regular } from "@expo-google-fonts/open-sans";
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    OpenSans_400Regular
+  })
+
+  if (!fontsLoaded) {
+    return null; // futurally, "null" will be replaced with a loading component
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-slate-300">
       <Text>Open up App.tsx to start working on your app!</Text>
