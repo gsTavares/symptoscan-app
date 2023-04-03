56b89f211afd1c4baacf2878215e3f04fcdc535c
Gustavo Tavares <gustavo.tavares24@outlook.com>
2023-04-02 22:28:09 -0300

    feat: add first screens

---
+++ src/screens/Register.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export const Register = () => {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text>Register</Text>
+        </View>
+    )
+}
