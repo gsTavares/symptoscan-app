4a38fc32336a1774c21541fd6a8dd19ac77235e4
gsTavares <gustavo.tavares24@outlook.com>
2023-04-09 21:53:27 -0300

    feat: add logout method

--- src/screens/Index.tsx
+++ src/screens/Index.tsx
@@ -1,9 +1,17 @@
-import { View, Text } from "react-native";
+import { useContext } from "react";
+import { View, Text, Pressable } from "react-native";
+import { AuthContext } from "../routes/util/auth.context";
 
 export const Index = () => {
+
+    const { logout } = useContext(AuthContext);
+
     return (
         <View className="flex-1 justify-center items-center">
             <Text>Index</Text>
+            <Pressable onPress={logout}>
+                <Text>Logout</Text>
+            </Pressable>
         </View>
     )
 }
