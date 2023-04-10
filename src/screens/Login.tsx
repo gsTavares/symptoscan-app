4a30f7f9e440190ed679fa80b372f24cabc6c020
gsTavares <gustavo.tavares24@outlook.com>
2023-04-09 21:52:31 -0300

    refac: form component margin from scroll view

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -6,7 +6,7 @@ export const Login = () => {
     return (
         <ScrollView className="bg-slate-200 h-full"
             showsVerticalScrollIndicator={false}
-            contentContainerStyle={{ marginTop: 20 }}>
+            contentContainerStyle={{ marginTop: 60 }}>
             <Form />
         </ScrollView>
     )
