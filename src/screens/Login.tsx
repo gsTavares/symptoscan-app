e89ba7eef4143354725f0c1bdca337e9154db6cf
gsTavares <gustavo.tavares24@outlook.com>
2023-04-13 12:42:06 -0300

    feat: margin adjustment

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -6,7 +6,7 @@ export const Login = () => {
     return (
         <ScrollView className="bg-slate-200 h-full"
             showsVerticalScrollIndicator={false}
-            contentContainerStyle={{ marginTop: 60 }}>
+            contentContainerStyle={{ marginTop: 70 }}>
             <Form />
         </ScrollView>
     )
