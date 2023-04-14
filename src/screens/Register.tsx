0be95b643be569c60447b1713235fa1be496ac8a
Mateus-Scarin <mateus.scarin@gmail.com>
2023-04-14 20:36:40 -0300

    feat: button return to login screen

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,9 +1,17 @@
-import { View, Text } from "react-native";
+import { EvilIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { Pressable, View } from "react-native";
+import colors from "tailwindcss/colors";
 
 export const Register = () => {
+    const navigation = useNavigation();
     return (
-        <View className="flex-1 justify-center items-center">
-            <Text>Register</Text>
+        <View className="mt-10 ml-3 flex-row justify-start">
+            <Pressable onPress={() => {
+                navigation.goBack();
+            }}>
+                <EvilIcons name="arrow-left" size={50} color={colors.blue[500]} />
+            </Pressable>
         </View>
     )
 }
